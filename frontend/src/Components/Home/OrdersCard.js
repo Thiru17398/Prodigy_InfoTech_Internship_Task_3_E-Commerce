6338472b19f5645d96bc9a2eb9e86e67479d1d79
Thiru <thirupandian11@gmail.com>
2024-07-13 13:21:08 +0530

    Orders Page Completed

--- frontend/src/Components/Home/OrdersCard.js
+++ frontend/src/Components/Home/OrdersCard.js
@@ -1,12 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Fetch } from '../Data/Fetch';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+
 
 const OrdersCard = ({order}) => {
   console.log(order);
